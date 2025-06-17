447295a8f6a36d11eef57514d510686f8b4ceccc
Mohammad Iqbal <ki925053@gmail.com>
2025-06-17 22:41:11 +0530

    ch

--- src/app/patients/[id]/page.tsx
+++ src/app/patients/[id]/page.tsx
@@ -1,6 +1,10 @@
 // app/patients/[id]/page.tsx
 import PatientDetailClient from "@/components/PatientDetailClient";
 
-export default function Page({ params }: { params: { id: string } }) {
-  return <PatientDetailClient id={params.id} />;
+export default async function Page({ params }: { params: { id: string } }) {
+  // --- This is the line that needs to be fixed ---
+  // const id = await Promise.resolve(params.id); // <--- REMOVE THIS LINE
+  const id = params.id; // <--- USE THIS LINE INSTEAD
+
+  return <PatientDetailClient id={id} />;
 }
